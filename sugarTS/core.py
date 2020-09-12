aef64af6a618ed5e7a1bff1a3edb16b23b45ef06
daniel wood <danielkentwood@gmail.com>
2020-09-11 20:45:27 -0500

    added streamlit app, cleaned code
    Added a streamlit app that demonstrates the functionality of the package.
    Cleaned the code.

--- sugarTS/core.py
+++ sugarTS/core.py
@@ -1,11 +1,14 @@
 import csv
 import datetime
+from collections import defaultdict
+from pathlib import Path
 
 import pandas as pd
 import numpy as np
+import sugarTS.constants as constants
 
 
-def get_datetime(df, var_name='EventDateTime', targ_name='dt'):
+def get_datetime(df, var_name="EventDateTime", targ_name="dt"):
     """
     Parse the datetime strings coming from the clarity and tandem exports.
     Replace the column of strings with a column of datetime objects
@@ -17,277 +20,307 @@ def get_datetime(df, var_name='EventDateTime', targ_name='dt'):
     Outputs:
     * df: dataframe with replaced datetime column
     """
+
     df[var_name] = df[var_name].apply(lambda x: x[:19])
-    df[targ_name] = df.apply(lambda row: datetime.datetime.strptime
-                             (row[var_name].replace
-                              ('T', ' '), '%Y-%m-%d %H:%M:%S'), axis=1)
+    df[targ_name] = df.apply(
+        lambda row: datetime.datetime.strptime(
+            row[var_name].replace("T", " "), "%Y-%m-%d %H:%M:%S"
+        ),
+        axis=1,
+    )
+
+    # df[targ_name] = df.apply(
+    #     lambda row: datetime.datetime.strptime(
+    #         row[var_name],
+    #         "%Y-%m-%dT%H:%M:%S"
+    #     ),
+    #     axis=1,
+    # )
+
     df = df.drop([var_name], axis=1)
     return df
 
 
-def load_tandem(tandem_fn):
-    '''
+def load_tandem(tandem_filename):
+    """
     Open the raw tandem file exported from the website.
 
     Inputs:
-    * tandem_fn: a file path (str) to the tandem file.
+    * tandem_filename: a file path (str) to the tandem file.
     Outputs:
     * tandem_dict: a dict containing 5 different measurements from the
     tandem pump device.
-    '''
+    """
+
     # grab the data from the raw tandem file
-    tandem_dict = {1: [], 2: [], 3: [], 4: [], 5: []}
-    with open(tandem_fn) as csv_file:
-        tandem_wb = csv.reader(csv_file, delimiter=',')
+    tandem_dict = defaultdict(list)
+    with open(tandem_filename) as csv_file:
+        tandem_wb = csv.reader(csv_file, delimiter=",")
         for row in tandem_wb:
-            if not row:
-                continue
-            else:
-                if 't:slim X2 Insulin Pump OUS' in row[0] and 'EGV' in row[2]:
-                    tandem_dict[1].append(row[:6])
-                if 't:slim X2 Insulin Pump OUS' in row[0] and 'BG' in row[2]:
-                    tandem_dict[2].append(row)
-                if 'IOB' in row[0]:
-                    tandem_dict[3].append(row[:4])
-                if 'Basal' in row[0]:
-                    tandem_dict[4].append(row[:3])
-                if 'Bolus' in row[0]:
-                    tandem_dict[5].append(row[:41])
+            if row:
+                if "t:slim X2 Insulin Pump OUS" in row[0] and "EGV" in row[2]:
+                    tandem_dict["estimated_glu"].append(row[:6])
+                if "t:slim X2 Insulin Pump OUS" in row[0] and "BG" in row[2]:
+                    tandem_dict["blood_glucose"].append(row)
+                if "IOB" in row[0]:
+                    tandem_dict["insulin_on_board"].append(row[:4])
+                if "Basal" in row[0]:
+                    tandem_dict["basal_rate"].append(row[:3])
+                if "Bolus" in row[0]:
+                    tandem_dict["bolus"].append(row[:41])
     return tandem_dict
 
 
 def shape_tandem(tandem_dict):
-    '''
-    Put the data into dataframes.
-    Drop unnecessary measurements.
+    """
+    Put the tandem data into dataframes.
+    Drop unnecessary columns.
 
     Inputs:
     * tandem_dict: a dict containing the tandem data.
     Outputs:
-    * egv: glucose values
-    * iob: inuslin-on-board
-    * br: basal rate of insulin
+    * estimated_glu: glucose values from continuous glucose monitor
+    * insulin_on_board: amount of insulin-on-board
+    * basal_rate: basal rate of background insulin administration
     * bolus: size of insulin bolus
-    '''
+    """
+
     # put the data into dataframes
-    egv = pd.DataFrame(tandem_dict[1], columns=['DeviceType', 'SerialNumber',
-                                                'Description', 'EventDateTime',
-                                                'bg', 'VOID'])
-    iob = pd.DataFrame(tandem_dict[3], columns=['Type', 'EventID',
-                                                'EventDateTime', 'iob'])
-    br = pd.DataFrame(tandem_dict[4], columns=['Type', 'EventDateTime', 'br'])
-    bolus = pd.DataFrame(tandem_dict[5],
-                         columns=['Type', 'BolusDescription',
-                                  'bg_bolus', 'iob_bolus',
-                                  'BolusRequestID',
-                                  'BolusCompletionID',
-                                  'CompletionDateTime',
-                                  'InsulinDelivered',
-                                  'FoodDelivered',
-                                  'CorrectionDelivered',
-                                  'CompletionStatusID',
-                                  'CompletionStatusDesc',
-                                  'BolusIsComplete',
-                                  'BolexCompletionID',
-                                  'BolexSize',
-                                  'BolexStartDateTime',
-                                  'BolexCompletionDateTime',
-                                  'BolexInsulinDelivered',
-                                  'BolexIOB',
-                                  'BolexCompletionStatusID',
-                                  'BolexCompletionStatusDesc',
-                                  'ExtendedBolusIsComplete',
-                                  'EventDateTime',
-                                  'RequestDateTime',
-                                  'BolusType',
-                                  'BolusRequestOptions',
-                                  'StandardPercent',
-                                  'Duration', 'CarbSize',
-                                  'UserOverride', 'TargetBG',
-                                  'CorrectionFactor',
-                                  'FoodBolusSize',
-                                  'CorrectionBolusSize',
-                                  'ActualTotalBolusRequested',
-                                  'IsQuickBolus',
-                                  'EventHistoryReportEventDesc',
-                                  'EventHistoryReportDetails',
-                                  'NoteID',
-                                  'IndexID', 'Note'])
+    estimated_glu = pd.DataFrame(
+        tandem_dict["estimated_glu"],
+        columns=constants.tandem_estimated_glu_columns,
+    )
+    insulin_on_board = pd.DataFrame(
+        tandem_dict["insulin_on_board"], columns=constants.tandem_iob_columns
+    )
+    basal_rate = pd.DataFrame(
+        tandem_dict["basal_rate"], columns=constants.tandem_basal_rate_columns
+    )
+    bolus = pd.DataFrame(
+        tandem_dict["bolus"],
+        columns=constants.tandem_bolus_columns,
+    )
+
     # shaping the continuous glucose monitor data
-    egv = egv.drop(['DeviceType', 'SerialNumber', 'Description', 'VOID'],
-                   axis=1)
-    egv = get_datetime(egv)
+    estimated_glu = estimated_glu.drop(
+        ["DeviceType", "SerialNumber", "Description", "VOID"], axis=1
+    )
+    estimated_glu = get_datetime(estimated_glu)
+    estimated_glu.rename(
+        columns={"bg": "estimated_glu"}, inplace=True)
+
     # shaping the basal rate data
-    br = br.drop(['Type'], axis=1)
-    br = get_datetime(br)
-    br = br.replace('', np.nan)
-    br.drop_duplicates(subset=None, keep='first', inplace=True)
-    br = br.dropna()
-    br.reset_index(drop=True, inplace=True)
+    basal_rate = basal_rate.drop(["Type"], axis=1)
+    basal_rate = get_datetime(basal_rate)
+    basal_rate = basal_rate.replace("", np.nan)
+    basal_rate.drop_duplicates(subset=None, keep="first", inplace=True)
+    basal_rate = basal_rate.dropna()
+    basal_rate.reset_index(drop=True, inplace=True)
+    basal_rate.rename(
+        columns={"br": "basal_rate"}, inplace=True)
+
     # shaping the insulin-on-board data
-    iob = iob.drop(['Type', 'EventID'], axis=1)
-    iob = get_datetime(iob)
-    iob = iob.replace('', np.nan)
+    insulin_on_board = insulin_on_board.drop(["Type", "EventID"], axis=1)
+    insulin_on_board = get_datetime(insulin_on_board)
+    insulin_on_board = insulin_on_board.replace("", np.nan)
+    insulin_on_board.rename(
+        columns={"iob": "insulin_on_board"}, inplace=True)
+
     # shaping the bolus data
     bolus = get_datetime(bolus)
-    bolus = bolus[['InsulinDelivered', 'dt']]
-    bolus.rename(columns={'InsulinDelivered': 'bolus_units'}, inplace=True)
+    bolus = bolus[["InsulinDelivered", "dt"]]
+    bolus.rename(
+        columns={"InsulinDelivered": "bolus"}, inplace=True)
 
-    return egv, br, iob, bolus
+    return estimated_glu, basal_rate, insulin_on_board, bolus
 
 
-def load_clarity(clarity_fn):
-    '''
+def load_clarity(clarity_filename):
+    """
     Open the raw clarity file exported from the website.
 
     Inputs:
-    * clarity_fn: a file path (str) to the clarity file.
+    * clarity_filename: a file path (str) to the clarity file.
+
     Outputs:
     * clarity: dataframe
-    '''
+    """
+
     # read and shape clarity data
-    clarity = pd.read_csv(clarity_fn)
-    return clarity
+    return pd.read_csv(clarity_filename)
 
 
 def shape_clarity(clarity):
-    '''
+    """
     Put the clarity data into dataframes.
     Drop unnecessary measurements.
 
     Inputs:
     * clarity: a dataframe containing raw clarity data.
+
     Outputs:
     * clarity: a clean dataframe with carb value measurements
-    '''
+    """
+
     # shape clarity data
-    clarity = clarity.drop(['Index'], axis=1)
+    clarity = clarity.drop(["Index"], axis=1)
     clarity = clarity[10:].reset_index(drop=True)
-    clarity = clarity[['Carb Value (grams)',
-                       'Timestamp (YYYY-MM-DDThh:mm:ss)']]
-    clarity = get_datetime(clarity, var_name='Timestamp (YYYY-MM-DDThh:mm:ss)',
-                           targ_name='dt')
-    clarity.rename(columns={'Carb Value (grams)': 'carb_grams'}, inplace=True)
+    clarity = clarity[["Carb Value (grams)",
+                       "Timestamp (YYYY-MM-DDThh:mm:ss)"]]
+    clarity = get_datetime(
+        clarity, var_name="Timestamp (YYYY-MM-DDThh:mm:ss)", targ_name="dt"
+    )
+    clarity.rename(columns={"Carb Value (grams)": "carb_grams"}, inplace=True)
+
     return clarity
 
 
-def combine_tandem_clarity(clarity, egv, br, iob, bolus):
-    '''
+def combine_tandem_clarity(
+    clarity, estimated_glu,
+    basal_rate, insulin_on_board, bolus
+):
+    """
     Combine the tandem and clarity data into a single pandas dataframe.
     Deal with empty values.
 
     Inputs:
     * clarity: dataframe with carb data from clarity device
-    * egv: dataframe with blood glucose data from tandem device
-    * br: dataframe with basal rate data from tandem device
-    * iob: dataframe with insulin on board data from tandem device
+    * estimated_glu: dataframe with blood glucose data from tandem device
+    * basal_rate: dataframe with basal rate data from tandem device
+    * insulin_on_board: dataframe with insulin on board data from tandem device
     * bolus: dataframe with bolus size data from tandem device
+
     Outputs:
     * data: combined dataframe
-    '''
+    """
+
     # find the timestamp to stop at
-    ts_lim = np.min([clarity.dt.max(), egv.dt.max()])
-    # concatenate the interesting columns: blood glucose (egv), basal rate,
-    # insulin-on-board, bolus amount, and carb amount.
-    d = pd.concat([egv, br, iob, bolus,
-                   clarity[['dt', 'carb_grams']]]).sort_values('dt')
+    timestamp_stop = np.min([clarity.dt.max(), estimated_glu.dt.max()])
+
+    # concatenate the interesting columns:
+    # estimated_glu, basal rate, insulin-on-board,
+    # bolus amount, and carb amount.
+    d = pd.concat(
+        [
+            estimated_glu,
+            basal_rate,
+            insulin_on_board,
+            bolus,
+            clarity[["dt", "carb_grams"]],
+        ]
+    ).sort_values("dt")
+
     # reset index
     d.reset_index(drop=True, inplace=True)
-    # replace empty values with NaN
-    d = d.replace('', np.nan)
-    # put the datetime column into the index
-    d.set_index('dt', inplace=True)
-    d = d.loc[:ts_lim]
+    d = d.replace("", np.nan)
+    d.set_index("dt", inplace=True)
+    d = d.loc[:timestamp_stop]
+
     # convert numeric values to float and resample
-    data = d[['bg', 'br', 'iob']].astype(float).resample('5T').mean()
-    data['carb_grams'] = pd.DataFrame(d['carb_grams'].astype(float).
-                                      resample('5T').sum())
-    data['bolus_units'] = pd.DataFrame(d['bolus_units'].astype(float).
-                                       resample('5T').sum())
+    data = (
+        d[["estimated_glu",
+           "basal_rate",
+           "insulin_on_board"]].astype(float).resample("5T").mean()
+    )
+    data["carb_grams"] = pd.DataFrame(
+        d["carb_grams"].astype(float).resample("5T").sum()
+    )
+    data["bolus"] = pd.DataFrame(
+        d["bolus"].astype(float).resample("5T").sum()
+    )
     # fill blood glucose and insulin-on-board with linearly interpolated values
-    data[['bg', 'iob']] = data[['bg',
-                                'iob']].interpolate(method='linear',
-                                                    limit_direction='forward',
-                                                    axis=0)
+    data[["estimated_glu", "insulin_on_board"]] = data[
+        ["estimated_glu", "insulin_on_board"]].interpolate(
+        method="linear", limit_direction="forward", axis=0
+    )
+
     # fill basal rate NaNs with forward fill (use the most recent valid value)
     # NOTE: This may be the wrong approach. It seems unlikely that there are
     # stretches of an hour or more where the basal rate is at 0. Look into
     # this further to see if interpolation of some sort is more appropriate.
-    data['br'] = data['br'].fillna(method='ffill')
-    # fill the NaNs in carb_grams with zeros
-    data['carb_grams'] = data['carb_grams'].replace(np.nan, 0)
-    # remove rows with leftover NaNs
+    data["basal_rate"] = data["basal_rate"].fillna(method="ffill")
+    data["carb_grams"] = data["carb_grams"].replace(np.nan, 0)
     data = data.drop(data.index[data.isnull().any(1)])
+
     return data
 
 
 def load_and_clean_example_data():
-    '''
+    """
     Load the raw example data from Clarity and Tandem.
     Then clean it.
+
     Outputs:
     * data: a dataframe containing:
-        - bg: blood glucose
-        - iob: insulin on board
-        - br: basal rate of insulin
+        - estimated_glu: blood glucose
+        - insulin_on_board: insulin on board
+        - basal_rate: basal rate of insulin
         - carb_grams: number of carbs in grams
-        - bolus_units: amount of insulin in bolus
-    '''
+        - bolus: amount of insulin in bolus
+    """
+
     # define paths
-    raw_dir = './examples/raw/'
-    clarity_fn = raw_dir+'CLARITY.csv'
-    tandem_fn = raw_dir+'TANDEM.csv'
-    # load and shape tandem
-    td_dict = load_tandem(tandem_fn)
-    egv, br, iob, bolus = shape_tandem(td_dict)
-    # load and shape clarity
-    clarity = load_clarity(clarity_fn)
-    clarity = shape_clarity(clarity)
-    # combine the two datasets
-    data = combine_tandem_clarity(clarity, egv, br, iob, bolus)
+    raw_dir = Path("./examples/raw/")
+    clarity_filename = raw_dir/"CLARITY.csv"
+    tandem_filename = raw_dir/"TANDEM.csv"
+
+    # load and clean example data
+    data = load_and_clean_data(clarity_filename, tandem_filename)
+
     return data
 
 
-def load_and_clean_data(clarity_fn, tandem_fn):
-    '''
+def load_and_clean_data(clarity_filename, tandem_filename):
+    """
     Load the raw data from Clarity and Tandem.
     Then clean it.
+
     Inputs:
-    * clarity_fn: full path for raw Clarity data
-    * tandem_fn: full path for raw Tandem data
+    * clarity_filename: full path for raw Clarity data
+    * tandem_filename: full path for raw Tandem data
+
     Outputs:
     * data: a dataframe containing:
-        - bg: blood glucose
-        - iob: insulin on board
-        - br: basal rate of insulin
+        - estimated_glu: blood glucose
+        - insulin_on_board: insulin on board
+        - basal_rate: basal rate of insulin
         - carb_grams: number of carbs in grams
-        - bolus_units: amount of insulin in bolus
-    '''
-    # load and shape tandem
-    td_dict = load_tandem(tandem_fn)
-    egv, br, iob, bolus = shape_tandem(td_dict)
-    # load and shape clarity
-    clarity = load_clarity(clarity_fn)
-    clarity = shape_clarity(clarity)
+        - bolus: amount of insulin in bolus
+    """
+
+    # load and shape tandem and clarity data
+    estimated_glu, basal_rate, insulin_on_board, bolus = shape_tandem(
+        load_tandem(tandem_filename)
+    )
+    clarity = shape_clarity(load_clarity(clarity_filename))
+
     # combine the two datasets
-    data = combine_tandem_clarity(clarity, egv, br, iob, bolus)
+    data = combine_tandem_clarity(
+        clarity, estimated_glu, basal_rate, insulin_on_board, bolus
+    )
     return data
 
 
 def split_data(data):
-    '''
+    """
     Split the data into training and testing sets.
+
     Input:
     * data: (dataframe) full dataset
+
     Outputs:
     * Xtrain: (dataframe)
     * ytrain: (series)
     * Xtest: (dataframe)
     * ytest: (series)
-    '''
-    split = int(len(data)*.75)
-    ytrain = data['bg'].iloc[:split]
-    Xtrain = data.loc[:, ['carb_grams', 'bolus_units']].iloc[:split, :]
-    ytest = data['bg'].iloc[split:]
-    Xtest = data.loc[:, ['carb_grams', 'bolus_units']].iloc[split:, :]
+    """
+
+    # Do a 75/25 split for train and test sets.
+    # No random sampling because it is a time series.
+    split = int(len(data) * 0.75)
+    ytrain = data["estimated_glu"].iloc[:split]
+    Xtrain = data.loc[:, ["carb_grams", "bolus"]].iloc[:split, :]
+    ytest = data["estimated_glu"].iloc[split:]
+    Xtest = data.loc[:, ["carb_grams", "bolus"]].iloc[split:, :]
+
     return Xtrain, ytrain, Xtest, ytest
--- sugarTS/model.py
+++ sugarTS/model.py
@@ -5,132 +5,171 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-from sugarTS.core import load_and_clean_example_data
-from sugarTS.core import split_data
+import sugarTS.core as core
 from fireTS.models import NARX
 
 
-class Patient():
-    '''
+class Patient:
+    """
     Object containing data, timeseries model, and forecasting for an
     individual patient.
-    '''
+    """
 
     def __init__(self):
         self.carbs_per_insulin = 5
-        self.data = load_and_clean_example_data()
         self.target_range = (80, 140)
 
+    def load_example_data(self):
+        self.data = core.load_and_clean_example_data()
+
+    def load_data(self, clarity_filename, tandem_filename):
+        self.data = core.load_and_clean_data(
+            clarity_filename, tandem_filename,
+        )
+
     def find_optimal_bolus(self, carbs, carb_t):
-        '''
+        """
         Use a brute force grid search to find the optimal
         timing and amount of insulin, given an amount and
         timing of carbs for a meal.
+
         Inputs:
         * carbs: amount of carbs in upcoming meal
         * carb_t: datetime object describing the timing of
                   the upcoming meal
+
         Output:
         * Optimal: dict containing:
                     * amount of time in range
                     * optimal bolus amount
                     * optimal bolus time
-        '''
-        carb_targ = int(carbs/self.carbs_per_insulin)
-        bolus_amounts = list(range(1, carb_targ+20, 1))
-        bolus_times = pd.date_range(start=self.Xtest.index[-1] +
-                                    timedelta(minutes=5),
-                                    end=carb_t, freq='5T')
+                    * grid from grid search
+        """
+
+        carb_targ = int(carbs / self.carbs_per_insulin)
+        bolus_amounts = list(range(0, carb_targ + 20))
+        bolus_times = pd.date_range(
+            start=self.Xtest.index[-1] + timedelta(minutes=5),
+            end=carb_t, freq="5T"
+        )
+
         # create grid for brute force grid search
-        grid = [(r[0], r[1]) for r in itertools.product(bolus_amounts,
-                                                        bolus_times)]
+        grid = list(itertools.product(bolus_amounts, bolus_times))
+
         # loop through permutations and check forecast against target_range
-        in_range = list()
+        in_range = []
         for b, b_t in grid:
             X_future = self.build_x_future(carbs, carb_t, b, b_t)
             ypred = self.forecast(X_future)
+
             # check performance
-            over = list(map(lambda x: x - self.target_range[1],
-                        [x for x in ypred if x > self.target_range[1]]))
-            under = list(map(lambda x: self.target_range[0] - x,
-                         [x for x in ypred if x < self.target_range[0]]))
+            over = list(
+                map(
+                    lambda x: x - self.target_range[1],
+                    [x for x in ypred if x > self.target_range[1]],
+                )
+            )
+            under = list(
+                map(
+                    lambda x: self.target_range[0] - x,
+                    [x for x in ypred if x < self.target_range[0]],
+                )
+            )
+
             # get the amount of error
-            error = sum([sum(over), sum(under)])
+            error = sum(itertools.chain(over, under))
             in_range.append(error)
+
         # get the feature pair that maximizes time in target range
         min_in_range = min(in_range)
         min_ind = [i for i, x in enumerate(in_range) if x == min_in_range]
         optimal_pair = grid[min_ind[0]]
+
         # handle potential errors
         if len(optimal_pair) == 0:
-            raise ValueError('Oops! Current bolus options do not result in ' +
-                             'forecasted blood glucose in target range.' +
-                             'Try new bolus options')
-            return -1
-        optimal = {'error': str(min_in_range),
-                   'bolus amount': optimal_pair[0],
-                   'bolus time': optimal_pair[1],
-                   'grid': grid}
+            raise ValueError(
+                "Oops! Current bolus options do not result in "
+                "forecasted blood glucose in target range."
+                "Try new bolus options"
+            )
+        optimal = {
+            "error": str(min_in_range),
+            "bolus amount": optimal_pair[0],
+            "bolus time": optimal_pair[1],
+            "grid": grid,
+        }
+
         return optimal
 
     def fit_model(self):
-        '''
+        """
         Fit a time-series model to the patient's data.
-        Currently only uses the fireTS model
-        '''
+        Currently only uses the fireTS model.
+        """
+
         # split data into train and test sets
-        Xtrain, ytrain, Xtest, ytest = split_data(self.data)
+        Xtrain, ytrain, Xtest, ytest = core.split_data(self.data)
         self.Xtrain = Xtrain
         self.ytrain = ytrain
         self.Xtest = Xtest
         self.ytest = ytest
+
         # initialize model object
-        narx_mdl = NARX(LinearRegression(),
-                        auto_order=30,
-                        exog_order=[60, 60],
-                        exog_delay=[0, 0])
+        narx_mdl = NARX(
+            LinearRegression(),
+            auto_order=30, exog_order=[60, 60], exog_delay=[0, 0]
+        )
+
         # fit model
         narx_mdl.fit(Xtrain, ytrain)
         self.model = narx_mdl
 
     def build_x_future(self, carbs, carb_t, bolus, bolus_t):
-        '''
+        """
         Build the future X matrix. This contains the future carbs and the
         future bolus.
+
         Inputs:
         * carbs: (int) number of future carbs to be eaten
         * carb_t: (datetime object) when the future carbs will be eaten
         * bolus: (int) units of insulin to administer
         * bolus_t: (datetime object) when the bolus will be given
+
         Output:
         * X_future: (dataframe) matrix containing future carbs and bolus.
-        '''
+        """
+
         # make tests to ensure that carb_t and bolus_t are datetime objects
         # make a test to ensure that bolus_t is between start time and carb_t
-        t_0 = self.Xtest.index[-1]+timedelta(minutes=5)
+        t_0 = self.Xtest.index[-1] + timedelta(minutes=5)
+
         # assume the end of the forecast is 1 hour after carb time
-        t_end = carb_t+timedelta(minutes=5*12)
-        ind = pd.date_range(start=t_0, end=t_end, freq='5T')
-        X_future = pd.DataFrame(np.zeros([len(ind), 2]),
-                                columns={'carb_grams', 'bolus_units'})
+        t_end = carb_t + timedelta(minutes=5 * 12)
+        ind = pd.date_range(start=t_0, end=t_end, freq="5T")
+        X_future = pd.DataFrame(
+            np.zeros([len(ind), 2]), columns={"carb_grams", "bolus_units"}
+        )
         X_future.index = ind
-        X_future.loc[carb_t, 'carb_grams'] = carbs
-        X_future.loc[bolus_t, 'bolus_units'] = bolus
+        X_future.loc[carb_t, "carb_grams"] = carbs
+        X_future.loc[bolus_t, "bolus_units"] = bolus
+
         return X_future
 
     def forecast(self, X_future):
-        '''
+        """
         Forecast the future blood glucose by supplying information about
         future food and future insulin.
+
         Input:
         * X_future: (dataframe) matrix containing future carbs and bolus.
-        Outpu:
+
+        Output:
         * yforecast_narx: (array-like) vector containing forecasted
         blood glucose values.
-        '''
-        steps = X_future.shape[0]+1
-        yforecast_narx = self.model.forecast(self.Xtest,
-                                             self.ytest,
-                                             step=steps,
-                                             X_future=X_future)
+        """
+
+        steps = X_future.shape[0] + 1
+        yforecast_narx = self.model.forecast(
+            self.Xtest, self.ytest, step=steps, X_future=X_future
+        )
         return yforecast_narx
